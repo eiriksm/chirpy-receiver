87a2c704c9784d9f6b038530a47464d2a2ecc95c
Eirik Stanghelle Morland <eirik@morland.no>
2025-05-03 22:23:06 +0200

    Browser version one (#7)

---
+++ receiver-web/src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import TestDecoder from './lib/TestDecoder.svelte';
+</script>
+
+<main>
+  <h1>Test Decoder</h1>
+  <TestDecoder filename="sound_assets/1.wav"/>
+  <p>Test decoder for the receiver web app
+</main>
---
+++ receiver-web/src/lib/TestDecoder.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { getStringFromBuffer } from '../../../src/decodeFromBuffer';
+  let props = $props();
+  const handleClick = async () => {
+    const response = await fetch(props.filename);
+    const buffer = await response.arrayBuffer();
+    const decodedString = await getStringFromBuffer(buffer);
+    console.log(decodedString);
+  };
+</script>
+
+<p>
+  { props.filename }
+  <button onclick={handleClick}>
+    Decode
+  </button>
+</p>
