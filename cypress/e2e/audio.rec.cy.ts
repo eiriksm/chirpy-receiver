e56f4068e34bfa9fb5772965ba91362fa6cf23ca
Eirik S. Morland <eirik@morland.no>
2025-05-08 12:28:06 +0200

    Add a test for the actual recording

---
+++ cypress/e2e/audio.rec.cy.ts
@@ -0,0 +1,11 @@
+describe('Main app with recording', () => {
+  it('Should have a record button in there', () => {
+    cy.visit('/receiver-web/dist/index.html')
+    cy.get('.audio-decoder').should('exist')
+    cy.get('.audio-decoder .start-record').click();
+    // Wait for 11 seconds, the duration of the recording.
+    cy.wait(11000);
+    cy.get('.audio-decoder .stop-record').click();
+    cy.get('.actual-decoded-message').contains('test')
+  })
+})
