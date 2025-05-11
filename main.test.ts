8c3a0baf69563b3b843bc07b34c877bb2c2901c8
Eirik S. Morland <eirik@morland.no>
2025-05-11 21:07:37 +0200

    Node test

--- main.test.ts
+++ main.test.ts
@@ -9,6 +9,11 @@ describe("main", () => {
     test('Should return the expected string', async () => {
         const file = 'tests/assets/1.wav'
         const result = await getStringFromFileName(file)
-        expect(result).toStrictEqual("test")
+        expect(result).toStrictEqual('test')
+    })
+    test('Should return the expected string from test 2', async () => {
+        const file = 'tests/assets/2.wav'
+        const result = await getStringFromFileName(file)
+        expect(result).toStrictEqual('🚀️')
     })
 })
