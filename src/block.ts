836e3e04188bb3afbdae7eefad9095c7bb33a5f5
Eirik Stanghelle Morland <eirik@morland.no>
2025-05-13 08:35:01 +0200

    More lint and prettier (#27)

--- src/block.ts
+++ src/block.ts
@@ -1,62 +1,57 @@
 class Block {
-    public nTones;
-    public startTonePos;
-    public bytes;
-    public ascii;
-    public crc;
-    public valid;
-    private utf8;
-    constructor(startTonePos, nTones, bytes, crc) {
-      this.startTonePos = startTonePos;
-      this.nTones = nTones;
-      this.bytes = bytes;
-      this.ascii = getAscii(bytes);
-      this.crc = crc;
-      this.valid = crc == getCRC8(bytes);
-      this.utf8 = this.decodeUtf8(bytes);
+  public nTones;
+  public startTonePos;
+  public bytes;
+  public ascii;
+  public crc;
+  public valid;
+  private utf8;
+  constructor(startTonePos, nTones, bytes, crc) {
+    this.startTonePos = startTonePos;
+    this.nTones = nTones;
+    this.bytes = bytes;
+    this.ascii = getAscii(bytes);
+    this.crc = crc;
+    this.valid = crc == getCRC8(bytes);
+    this.utf8 = this.decodeUtf8(bytes);
+  }
+  decodeUtf8(bytes: Array<number>) {
+    let str = "";
+    try {
+      const decoder = new TextDecoder("utf-8");
+      str = decoder.decode(new Uint8Array(bytes));
+    } catch (e) {
+      console.log("Error decoding UTF-8: " + e);
     }
-    decodeUtf8(bytes: Array<number>) {
-      let str = ""
-      try {
-        const decoder = new TextDecoder("utf-8");
-        str = decoder.decode(new Uint8Array(bytes));
-      } catch (e) {
-        console.log("Error decoding UTF-8: " + e);
-      }
-      return str;
-    }
-    getUtf8() {
-      return this.utf8;
-    }
+    return str;
+  }
+  getUtf8() {
+    return this.utf8;
+  }
 }
-
 
 function getAscii(bytes) {
-    let res = "";
-    for (const b of bytes) {
-      res += String.fromCodePoint(b);
-    }
-    return res;
+  let res = "";
+  for (const b of bytes) {
+    res += String.fromCodePoint(b);
+  }
+  return res;
 }
-
 
 function getCRC8(bytes) {
+  let crc = 0;
+  for (const b of bytes) crc = updateCRC(b, crc);
+  return crc;
 
-    let crc = 0;
-    for (const b of bytes)
-      crc = updateCRC(b, crc);
-    return crc;
-
-    function updateCRC(nextByte, crc) {
-      for (let j = 0; j < 8; j++) {
-        let mix = (crc ^ nextByte) & 0x01;
-        crc >>= 1;
-        if (mix)
-          crc ^= 0x8C;
-        nextByte >>= 1;
-      }
-      return crc;
+  function updateCRC(nextByte, crc) {
+    for (let j = 0; j < 8; j++) {
+      const mix = (crc ^ nextByte) & 0x01;
+      crc >>= 1;
+      if (mix) crc ^= 0x8c;
+      nextByte >>= 1;
     }
+    return crc;
+  }
 }
 
-export { Block }
+export { Block };
