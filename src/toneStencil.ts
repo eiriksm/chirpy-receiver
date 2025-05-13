83064f7bd8f96070a0650b094d9c00f2b74434ab
Eirik S. Morland <eirik@morland.no>
2025-05-13 20:27:10 +0200

    Add rest of these?

--- src/toneStencil.ts
+++ src/toneStencil.ts
@@ -1,28 +1,29 @@
 class ToneStencil {
-    public freq;
-    public bins;
-    constructor(freq: number, sampleRate: number, fftSize: number) {
-      this.freq = freq;
-      this.bins = getBins(freq, sampleRate, fftSize, true);
+  public freq;
+  public bins;
+  constructor(freq: number, sampleRate: number, fftSize: number) {
+    this.freq = freq;
+    this.bins = getBins(freq, sampleRate, fftSize, true);
+  }
+}
+
+function getBins(
+  freq: number,
+  sampleRate: number,
+  fftSize: number,
+  multiple: boolean = false,
+) {
+  const bandwidth = sampleRate / fftSize;
+  let midIx = -1;
+  for (let i = 0; i < fftSize / 2; ++i) {
+    if (freq > i * bandwidth && freq <= (i + 1) * bandwidth) {
+      midIx = i;
+      break;
     }
   }
+  if (multiple) {
+    return [midIx - 1, midIx, midIx + 1];
+  } else return [midIx];
+}
 
-
-
-  function getBins(freq: number, sampleRate: number, fftSize: number, multiple : boolean = false) {
-    const bandwidth = sampleRate / fftSize;
-    let midIx = -1;
-    for (let i = 0; i < fftSize / 2; ++i) {
-      if (freq > i * bandwidth && freq <= (i+1) * bandwidth) {
-        midIx = i;
-        break;
-      }
-    }
-    if (multiple) {
-      return [midIx - 1, midIx, midIx + 1];
-    }
-    else return [midIx];
-  }
-
-
-export { ToneStencil }
+export { ToneStencil };
