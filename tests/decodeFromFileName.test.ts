d36e9c9a8337ae8f0247c853b8b3bcee8e1ed1fd
Eirik S. Morland <eirik@morland.no>
2025-05-16 07:04:16 +0200

    prettier

--- tests/decodeFromFileName.test.ts
+++ tests/decodeFromFileName.test.ts
@@ -1,19 +1,22 @@
-import { getRawStringBlocksFromFileName, getStringFromFileName } from "../src/decodeFromFileName"
+import {
+  getRawStringBlocksFromFileName,
+  getStringFromFileName,
+} from "../src/decodeFromFileName";
 
 describe("main", () => {
-    test('Should return the expected string blocks', async () => {
-        const file = 'tests/assets/1.wav'
-        const result = await getRawStringBlocksFromFileName(file, 32)
-        expect(result).toStrictEqual(["test\n"])
-    })
-    test('Should return the expected string', async () => {
-        const file = 'tests/assets/1.wav'
-        const result = await getStringFromFileName(file, 32)
-        expect(result).toStrictEqual('test')
-    })
-    test('Should return the expected string from test 2', async () => {
-        const file = 'tests/assets/2.wav'
-        const result = await getStringFromFileName(file, 32)
-        expect(result).toStrictEqual('🚀️')
-    })
-})
+  test("Should return the expected string blocks", async () => {
+    const file = "tests/assets/1.wav";
+    const result = await getRawStringBlocksFromFileName(file, 32);
+    expect(result).toStrictEqual(["test\n"]);
+  });
+  test("Should return the expected string", async () => {
+    const file = "tests/assets/1.wav";
+    const result = await getStringFromFileName(file, 32);
+    expect(result).toStrictEqual("test");
+  });
+  test("Should return the expected string from test 2", async () => {
+    const file = "tests/assets/2.wav";
+    const result = await getStringFromFileName(file, 32);
+    expect(result).toStrictEqual("🚀️");
+  });
+});
