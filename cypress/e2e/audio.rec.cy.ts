52356485728544115f98a80c6fd37171671848ae
Eirik S. Morland <eirik@morland.no>
2025-08-19 20:51:07 +0200

    Click that button

--- cypress/e2e/audio.rec.cy.ts
+++ cypress/e2e/audio.rec.cy.ts
@@ -1,6 +1,8 @@
 describe('Main app with recording', () => {
   it('Should have a record button in there', () => {
     cy.visit('/receiver-web/dist/index.html')
+    // We also need to click the 32 Hz button
+    cy.get('button#rate-32').click();
     cy.get('.audio-decoder').should('exist')
     cy.get('.audio-decoder .start-record').click();
     // Wait for 11 seconds, the duration of the recording.
